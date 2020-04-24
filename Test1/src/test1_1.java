500738aa0b412d900e9beea51e7b9dbe5fb52731
tw180021104320 <382065219@qq,com>
2020-04-24 12:09:09 +0800

    first commit

---
+++ Test1/src/test1_1.java
@@ -0,0 +1,13 @@
+
+public class test1_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=6;
+		for(int i=1;i<n;i++) {
+			System.out.print(i);
+		}
+		System.out.println(n);
+	}
+
+}
